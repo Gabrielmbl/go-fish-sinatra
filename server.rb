a905f47ae10aaaa90530366d953b67b0115fdca6
Gabrielmbl <gabrielmiranda7822@gmail.com>
2024-06-24 12:40:53 -0400

    List of names gets updated on form post

--- server.rb
+++ server.rb
@@ -11,11 +11,34 @@ require 'rack/contrib'
 #   use Rack::JSONBodyParser
 # end
 
+
 class Server < Sinatra::Base
+  enable :sessions
+  register Sinatra::RespondWith
+  use Rack::JSONBodyParser
+
+  players = []
+
   def self.game
     @@game ||= Game.new
   end
+
   get '/' do
+    @players = players
     slim :index
   end
+
+  post '/join' do
+    player = params['name']
+    session[:current_player] = player
+    # self.class.game.add_player(player)
+    players << player unless players.include?(player)
+    redirect '/'
+    # redirect '/game'
+  end
+
+  # get '/game' do
+  #   redirect '/' if self.class.game.empty?
+  #   slim :game, locals: { game: self.class.game, current_player: session[:current_player] }
+  # end
 end
