213d38adca90a3c73e2269f4ba2bd8477a14e5ce
Gabrielmbl <gabrielmiranda7822@gmail.com>
2024-06-25 08:36:41 -0400

    Passed test to bold only the current player and show their api key

--- spec/features/join_game_spec.rb
+++ spec/features/join_game_spec.rb
@@ -54,4 +54,23 @@ RSpec.describe Server do
     }
     expect(JSON.parse(last_response.body).keys).to include 'players'
   end
+
+  it 'bolds only the current player and shows their API key' do
+    session1 = Capybara::Session.new(:rack_test, Server.new)
+    session2 = Capybara::Session.new(:rack_test, Server.new)
+    [session1, session2].each_with_index do |session, index|
+      player_name = "Player #{index + 1}"
+      session.visit '/'
+      session.fill_in :name, with: player_name
+      session.click_on 'Join'
+      expect(session).to have_content('Players')
+    end
+    expect(session2).to have_content('Player 1')
+    expect(session1).to have_css('b', count: 1)
+    expect(session2).to have_css('b', count: 1)
+    expect(session1).to have_content('API Key', count: 1)
+  end
+
+  it 'returns 401 if API key is invalid' do
+  end
 end
