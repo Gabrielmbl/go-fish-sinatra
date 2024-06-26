7df341faaa38158656c48ad193feff0f48b06224
Gabrielmbl <gabrielmiranda7822@gmail.com>
2024-06-26 11:35:41 -0400

    Refactored game.slim

--- spec/features/server_spec.rb
+++ spec/features/server_spec.rb
@@ -109,15 +109,7 @@ RSpec.describe Server do
   end
 
   it 'bolds only the current player and shows their API key' do
-    session1 = Capybara::Session.new(:rack_test, Server.new)
-    session2 = Capybara::Session.new(:rack_test, Server.new)
-    [session1, session2].each_with_index do |session, index|
-      player_name = "Player #{index + 1}"
-      session.visit '/'
-      session.fill_in :name, with: player_name
-      session.click_on 'Join'
-      expect(session).to have_content('Players')
-    end
+    session1, session2 = create_sessions_and_players
     expect(session2).to have_content('Player 1')
     expect(session1).to have_css('b', count: 1)
     expect(session2).to have_css('b', count: 1)
